db0a426baa5440e34cfdad7f7302d4a40001f022
CNjacob <15375187600@163.com>
2020-05-28 11:33:39 +0800

    objc4-781

---
+++ RuntimeTest/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  RuntimeTest
+//
+//  Created by jacob on 2020/5/26.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+        NSObject *objc = [NSObject alloc];
+        
+    }
+    return 0;
+}
